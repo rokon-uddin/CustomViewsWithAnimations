1b3de98d642f2bb93fe8b5da4ed257f3185b47b6
Mohammed Rokon Uddin <rokon.uddin@monstar-lab.com>
2024-07-24 13:04:35 +0600

    Initial Commit

---
+++ CustomViewsWithAnimations/CustomViewsWithAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewsWithAnimationsApp.swift
+//  CustomViewsWithAnimations
+//
+//  Created by Mohammed Rokon Uddin on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomViewsWithAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
